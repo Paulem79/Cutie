713ac5212ff6f1e3a63653d8a132aedc3ba08f84
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-11-05 23:34:35 +0100

    refactor: updated to ModCutie

--- src/main/java/net/paulem/cutie/platform/neoforge/NeoforgeClientEventSubscriber.java
+++ src/main/java/net/paulem/cutie/platform/neoforge/NeoforgeClientEventSubscriber.java
@@ -2,17 +2,17 @@ package net.paulem.cutie.platform.neoforge;
 
 //? neoforge {
 /*
-import net.paulem.cutie.ModTemplate;
+import net.paulem.cutie.ModCutie;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = ModTemplate.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = ModCutie.MOD_ID, value = Dist.CLIENT)
 public class NeoforgeClientEventSubscriber {
 	@SubscribeEvent
 	public static void onClientSetup(final FMLClientSetupEvent event) {
-		ModTemplate.onInitializeClient();
+		ModCutie.onInitializeClient();
 	}
 }
 *///?}
--- src/main/java/net/paulem/cutie/platform/neoforge/NeoforgeEntrypoint.java
+++ src/main/java/net/paulem/cutie/platform/neoforge/NeoforgeEntrypoint.java
@@ -2,14 +2,14 @@ package net.paulem.cutie.platform.neoforge;
 
 //? neoforge {
 /*
-import net.paulem.cutie.ModTemplate;
+import net.paulem.cutie.ModCutie;
 import net.neoforged.fml.common.Mod;
 
-@Mod(ModTemplate.MOD_ID)
+@Mod(ModCutie.MOD_ID)
 public class NeoforgeEntrypoint {
 
 	public NeoforgeEntrypoint() {
-		ModTemplate.onInitialize();
+		ModCutie.onInitialize();
 	}
 }
 *///?}
